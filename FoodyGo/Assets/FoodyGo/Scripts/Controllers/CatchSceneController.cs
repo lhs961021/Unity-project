38f182226c159107e0ca94cc169bbccd00c02c8e
lhs961021 <lhs961021@naver.com>
2021-08-25 01:36:44 +0900

    inventory 구현 전까지

--- FoodyGo/Assets/FoodyGo/Scripts/Controllers/CatchSceneController.cs
+++ FoodyGo/Assets/FoodyGo/Scripts/Controllers/CatchSceneController.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using packt.FoodyGO.Services;
+using packt.FoodyGO.Database;
+using packt.FoodyGO.Managers;
 
 namespace packt.FoodyGO.Controllers
 {
@@ -9,18 +12,58 @@ namespace packt.FoodyGO.Controllers
         public MonsterController monster;
         public GameObject[] frozenDisableList;
         public GameObject[] frozenEnableList;
-                
+
+        public Transform escapeParticlePrefab;
+        public float monsterChanceEscape;
+        public float monsterWarmRate;
+        public bool catching;
+        public Monster monsterProps;
+
+        void Start()
+        {
+            monsterProps = MonsterFactory.CreateRandomMonster();
+            print(monsterProps);
+
+            monsterChanceEscape = monsterProps.Power * monsterProps.Level;
+            monsterWarmRate = .0001f * monsterProps.Power;
+            catching = true;
+
+            StartCoroutine(CheckEscape());
+        }
+
+        IEnumerator CheckEscape()
+        {
+            while (catching)
+            {
+                yield return new WaitForSeconds(30);
+                if (Random.Range(0, 100) < monsterChanceEscape && monster!= null)
+                {
+                    catching = false;
+                    print("Monster ESCAPED");
+                    monster.gameObject.SetActive(false);
+                    Instantiate(escapeParticlePrefab);
+                    foreach (var g in frozenDisableList)
+                    {
+                        g.SetActive(false);
+                    }
+                }
+            }
+        }  
+
         public void OnMonsterHit(GameObject go, Collision collision)
         {
             monster = go.GetComponent<MonsterController>();
             if (monster != null)
             {
+                monster.monsterWarmRate = monsterWarmRate;
                 print("Monster hit");
                 var animSpeedReduction = Mathf.Sqrt(collision.relativeVelocity.magnitude) / 10;
                 monster.animationSpeed = Mathf.Clamp01(monster.animationSpeed - animSpeedReduction);
                 if (monster.animationSpeed == 0)
                 {
                     print("Monster FROZEN");
+                    InventoryService.Instance.CreateMonster(monsterProps);
+
 
                     Instantiate(frozenParticlePrefab);
 
